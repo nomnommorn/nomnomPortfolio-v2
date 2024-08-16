969e87ec86277c7a9619bbc69e36c42f79b49bce
asaco <asqen4101@gmail.com>
2024-08-17 02:29:30 +0900

    [Update] Profile page

---
+++ pages/profile.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>Profile</h1>
+
+<h2>asaco/sacco</h2>
+
+<NuxtImg format="webp" src="../public/images/ME_p_858x1280.jpg" />
+
+    <p>
+        Kyoto, Japan<br>
+        A technical director and a web/installation engineer specializing in AR (Augmented Reality).<br>
+        Performing VJ with TouchDesigner and also experimenting on visual expression with node-based visual programming such as SparkAR, Unity, and cables.
+    </p>
+
+    <p>
+        テクニカルディレクター, web/インスタレーションエンジニア。得意分野はAR(拡張現実)。<br>
+        TouchDesignerを使ったVJ活動とともに、SparkARやUnity、cablesといったノードベースのプログラミングなどを使いARをはじめさまざまな視覚表現を実験中。
+    </p>
+</template>
