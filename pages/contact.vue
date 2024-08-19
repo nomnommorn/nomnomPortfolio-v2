cb5d6f815d2bb65a2dd874ecd71c78676e5bd28a
asaco <asqen4101@gmail.com>
2024-08-20 00:42:28 +0900

    [Update] contact page

---
+++ pages/contact.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="contact">
+        <h1>Contact</h1>
+        <div class="contact_wrap">
+        <ul>
+            <li>
+                <img class="mail icon" src="../public/images/icon/mail_icon.svg" alt="mail" />
+                <h2 class="mail_address">nomnommorn@gmail.com</h2>
+            </li>
+
+            <li>
+                <a href="https://www.instagram.com/nomnommorn_cr/" target="_blank">
+                    <img class="ig icon" src="../public/images/icon/IG_icon.svg" alt="Instagram" />
+                    <h2 class="ig_account">@nomnommorn_cr</h2>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/nomnommorn/" target="_blank">
+                    <img class="ig icon" src="../public/images/icon/IG_icon.svg" alt="Instagram" />
+                    <h2 class="ig_account">@nomnommorn</h2>
+                </a>
+            </li>
+            <li>
+                <a  href="https://x.com/nomnommorn" target="_blank">
+                    <img class="ig icon" src="../public/images/icon/x_icon.svg" alt="Instagram" />
+                    <h2 class="ig_account">@nomnommorn</h2>
+                </a>
+            </li>
+        </ul>
+    </div>
+    </div>
+</template>
+
+<style lang="scss" scope>
+.contact {
+    &_wrap {
+        width: 100%;
+        margin: 0 auto;
+        @media (max-width: $spwidth) {
+            margin: 13px auto 0;
+        }
+        ul {
+            text-align: center;
+            width: 350px;
+            display: block;
+            margin: 0 auto;
+            list-style: none;
+            li {
+                margin: 0 auto 120px;
+                .icon {
+                    width: auto;
+                    height: 150px;
+                    margin: 0 auto;
+                }
+            }
+        }
+    }
+}
+</style>
