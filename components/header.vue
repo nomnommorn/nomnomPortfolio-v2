23f9b1d13262cbb35e500d0ccf3cdff48630f6df
asaco <asqen4101@gmail.com>
2024-08-20 00:00:26 +0900

    [Update] header menu (click modal func.) and style

--- components/header.vue
+++ components/header.vue
@@ -5,7 +5,7 @@
           <a href="/">nomnommorn</a>
         </h1>
         <div id="menuarea">
-          <ul id="menu">
+          <ul v-show="open" id="menu">
               <li>
                 <a href="/">
                   Top 
@@ -27,15 +27,27 @@
               </a>
             </li>
           </ul>
-          <svg class="downarw" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="512px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" xml:space="preserve">
-            <polygon points="396.6,160 416,180.7 256,352 96,180.7 115.3,160 256,310.5 "/>
-          </svg>
+          <button :class="{open}" class="btn_wrap" @click="toggle">
+            <svg class="btn_arw" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="512px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" xml:space="preserve">
+              <polygon points="396.6,160 416,180.7 256,352 96,180.7 115.3,160 256,310.5 "/>
+            </svg>
+          </button>
         </div>
       </div>
     </header>
   </template>
   
-  <style>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const open = ref(false);
+
+const toggle = () => {
+  open.value = !open.value
+}
+</script>
+
+<style lang="scss" scope>
     header {
       position: fixed;
       display: inline-block;
@@ -45,7 +57,7 @@
       z-index: 11;
       top: 0;
       left: 0;
-      @media (max-width: 600px) {
+      @media (max-width: $spwidth) {
         height: 140px;
       }
     #menuwrap {
@@ -60,7 +72,7 @@
         margin: 20px auto 0;
         width: fit-content;
         height: 75px;
-        @media (max-width: 600px) {
+        @media (max-width: $spwidth) {
           font-size: 2rem;
           height: 50px;
           margin: 30px auto 0;
@@ -80,38 +92,48 @@
         text-align: center;
         z-index: 100;
         cursor: pointer;
-        @media (max-width: 600px) {
+        @media (max-width: $spwidth) {
           font-size: 1rem;
           margin: 0 auto;
         }
-        .downarw {
+        .btn_wrap {
           display: block;
           margin: 0 auto;
           width: 30px;
-          height: auto;
-          fill: #fff;
+          height: 30px;
+          background: transparent;
+          border: none;
+          transition: all .3s;
+          .btn_arw {
+            height: 100%;
+            fill: #fff;
+            cursor: pointer;
+          }
         }
-        .rotate {
-          transform: rotate(180deg);
-          transition: transform .5s;
+        .open {
+          animation: arrowchange .3s;
+          animation-fill-mode: both;
         }
-        .rotate_re {
-          transform: rotate(0deg);
-          transition: transform .5s;
+        @keyframes arrowchange {
+          0% {
+              transform: rotate(0deg);
+          }
+          100% {
+              transform: rotate(180deg) translateX(-10px);
+          }
         }
         #menu {
-          display: none;
           width: fit-content;
           margin: 0 auto;
           padding: 0;
-          @media (max-width: 600px) {
+          @media (max-width: $spwidth) {
             margin: 0 auto 5px;
           }
           li {
             list-style: none;
             display: inline-block;
             margin-left: 75px;
-            @media (max-width: 600px) {
+            @media (max-width: $spwidth) {
               margin-left: 20px;
             }
           }
@@ -122,4 +144,4 @@
       }
     }
   }
-  </style>
+</style>
